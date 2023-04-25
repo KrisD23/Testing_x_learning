e02a897c40aae081ca8c15b42d2767b7ff50e9ae
Krishan Dwivedi <krishandwive9@gmail.com>
2023-04-25 22:53:11 +0530

    Added a way to change bg Color

--- lib/main.dart
+++ lib/main.dart
@@ -13,30 +13,61 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var textSize = 3.00;
+  var textSize = 10.00;
+  var bgColor = 0; 
+  List <Color> colors = [Colors.black,Colors.white,Colors.amber,Colors.pinkAccent];
   void fontIncrease() {
-    
     setState(() {
-      textSize ++;
+      textSize++;
+    });
+  }
+  void colorLeft (){
+    setState(() {
+      bgColor--;
+    });
+  }
+  void colorRight (){
+    setState(() {
+      bgColor++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-             Text(
-              "Test",
-              style: TextStyle(fontSize: textSize),
-            ),
-            OutlinedButton.icon(
-              onPressed: fontIncrease,
-              icon: const Icon(Icons.add_to_photos_sharp),
-              label: const Text('Press me'),
-            ),
-          ],
+      home: Scaffold( appBar: AppBar(backgroundColor: colors[1]),
+      backgroundColor: colors[bgColor],
+      
+        body: Center(
+          child: Column(mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Hello world!",
+                style: TextStyle(fontSize: textSize),
+              ),
+              OutlinedButton.icon(
+                onPressed: fontIncrease,
+                icon: const Icon(Icons.add_to_photos_sharp),
+                label: const Text('Increase'),
+              ),
+
+              //Color Change:
+              const SizedBox(
+                height: 23,
+              ),
+              Row(mainAxisSize: MainAxisSize.min,
+                children: [
+                  FloatingActionButton.small(
+                      onPressed: colorLeft,
+                      child: const Icon(Icons.arrow_left)),
+                  const Text(' Change Color of the Screen ',style: TextStyle(fontSize: 16),),
+                  FloatingActionButton.small(
+                      onPressed: colorRight,
+                      child: const Icon(Icons.arrow_right))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
